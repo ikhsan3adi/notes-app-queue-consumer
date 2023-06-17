f0703a19088dc0c342d69845ac9d44eac93ed6a8
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2023-06-17 16:49:18 +0700

    consumer program finished

---
+++ src/MailSender.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Catatan',
+      text: 'Terlampir hasil dari ekspor catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
